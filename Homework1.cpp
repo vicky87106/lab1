41e26ca74ea21f272e5ac075136966b042bb0dab
vicky87106 <vicky87106@gmail.com>
2018-03-15 13:17:33 +0800

    Title This is about Lab1,Question B. It is about Writing a C++ program to read in an integer and print out the corresponding sequence of numbers.
    Since we will do something different when the integer is odd and even,
    so I use if else statement to make the condition different.
    Since we want to repeat the if else statement,
    so I use do while statement to make it repeat.

---
+++ Homework1.cpp
@@ -0,0 +1,28 @@
+
+/*Write a C++ program to read in an integer and print out the corresponding sequence of numbers.*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	  cout <<"Please enter an integer: ";
+          cin >> n;
+          cout << "The result is "<< n << " ";
+          do
+	  {
+	    if( n%2 == 1 && n != 1) 
+	       {
+	         n = 3*n + 1;
+                 cout << n << " ";
+	       } 
+	    else
+	       {
+                n = n / 2;
+                cout<< n << " ";
+               }	
+      
+        }while (n != 1);
+       
+	return 0;
+
+}
